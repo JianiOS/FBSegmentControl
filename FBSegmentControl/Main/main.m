80aaccda66f67f1e81380f56e401ff332e5a1c28
尹健 <jian.yin@fenbeitong.com>
2016-06-20 10:21:42 +0800

    Init Version

---
+++ FBSegmentControl/Main/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FBSegmentControl
+//
+//  Created by 尹健 on 16/6/20.
+//  Copyright © 2016年 FenbeiTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ FBSegmentControl/SRC/FBSegmentControl.m
@@ -0,0 +1,21 @@
+//
+//  FBSegmentControl.m
+//  FBSegmentControl
+//
+//  Created by 尹健 on 16/6/20.
+//  Copyright © 2016年 FenbeiTech. All rights reserved.
+//
+
+#import "FBSegmentControl.h"
+
+@implementation FBSegmentControl
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
